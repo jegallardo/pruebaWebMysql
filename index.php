50e47e071bbb5f817167e6722f6e11a44f0605cf
jegallardo <jgallardo@kssoluciones.com.mx>
2015-02-09 11:54:18 -0600

    cambio de prueba
    cambio de prueba

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 <?php
   
 
-$link =new mysqli_connect('localhost','KS02','Kmadminks02','sian')
+$link =new mysqli_connect('127.0.0.1','KS02','Kmadminks02','sian')
  or die('No se pudo conectar: ' );
 echo 'Connected successfully';
 
-mysql_select_db($link,'sian') or die('No se pudo seleccionar la base de datos');
+mysql_select_db('sian',$link) or die('No se pudo seleccionar la base de datos');
 
 
 // Realizar una consulta MySQL
